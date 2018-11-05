5a53316e875ed834569273706723a4bb2a3c74cf
PluxLight <aeoragy@gmail.com>
2018-11-05 19:54:23 +0900

    간소화

--- project/app/views.py
+++ project/app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 import json
 from app import button
 from app import crawcafeteria
+from app import db_control
 
 
 def keyboard(request):
@@ -11,7 +12,7 @@ def keyboard(request):
     return  JsonResponse(
         {
             "type": "buttons",
-            "buttons": ["채움관", "이룸관", "기숙사", "별점 확인&주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
         }
     )
 
@@ -24,10 +25,165 @@ def message(request):
     #content_name_jeon = received_json['content'][1]
     user_name = received_json['user_key']
 
-    input_text = button.button_check(content_name)
+    pre_input_text = db_control.pre_value(user_name)
 
-    if input_text == '내일':
-        return JsonResponse(
+    pre_pre_input_text = db_control.pre_pre_value(user_name)
+
+    db_control.key_insert(user_name, content_name)
+
+    #print(pre_input_text)
+    #print(pre_pre_input_text)
+
+    #input_text = button.button_check(content_name)
+
+    throw_message = button.message_make(content_name, pre_input_text, pre_pre_input_text)
+
+    return throw_message.return_message()
+
+
+#사진 전송을 로컬의 이미지를 업로드 하는 기능은 제공하지 않고 있음
+#사진 전송 기능 사용하려면 별도로 웹에 올려둬야함
+#플러스친구 홈페이지에 올리면 되지않나 고민중
+
+
+def trash():
+    """
+        elif input_text == '사진 전송':
+            return JsonResponse(
+                {
+                    'message': {
+                        'text': input_text,
+                        'photo' : {
+                            "url" : "http://i1.ruliweb.com/cmt/18/11/03/166d9718352368a8a.jpg", #"C:\project/est_grim.jpg",
+                            "width": 720,
+                            "height":630
+                        }
+                    },
+                    "keyboard": {
+                        "type": "buttons",
+                        "buttons": ["채움관", "이룸관", "기숙사", "별점 확인&주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인","사진 전송"]
+                    }
+                }
+            )
+    """
+
+    """
+        if content_name == '별점 확인&주기' or "학식(중식)" or "학식(석식)" or "기숙사(조식)" or "기숙사(중식)" or "기숙사(석식)" or "처음으로":
+            input_text = button.star_check(content_name)
+
+            if input_text == "별점":
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '평가할 식단을 선택하세요' #+ str(content_name_jeon)
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["학식(중식)", "학식(석식)", "기숙사(조식)", "기숙사(중식)", "기숙사(석식)", "처음으로"]
+                        }
+                    }
+                )
+            elif input_text == "채움이룸_중식":
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 매겨주세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
+                        }
+                    }
+                )
+            elif input_text == "채움이룸_석식":
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 매겨주세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
+                        }
+                    }
+                )
+            elif input_text == "기숙사_조식":
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 매겨주세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
+                        }
+                    }
+                )
+            elif input_text == "기숙사_중식":
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 매겨주세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
+                        }
+                    }
+                )
+            elif input_text == "기숙사_석식":
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 매겨주세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
+                        }
+                    }
+                )
+            else:
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': input_text
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 확인&주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+        elif content_name == '채움관' :
+            input_text = button.button_check(content_name)
+
+            if input_text == '내일':
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': '식당을 선택'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관(내일)", "이룸관(내일)", "기숙사(내일)", "처음으로"]
+                        }
+                    }
+                )
+            else:
+                return JsonResponse(
+                    {
+                        'message': {
+                            'text': input_text
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 확인&주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+    
+    a = JsonResponse(
             {
                 'message': {
                     'text': '식당을 선택'
@@ -38,11 +194,10 @@ def message(request):
                 }
             }
         )
-    else:
-        return JsonResponse(
+    b = JsonResponse(
             {
                 'message': {
-                    'text': input_text # + ' ' + str(content_name) + '\n' + str(user_name)
+                    'text': input_text + ' \n' + str(pre_input_text)
                 },
                 "keyboard": {
                     "type": "buttons",
@@ -50,133 +205,11 @@ def message(request):
                 }
             }
         )
+    if input_text == '내일':
+        return a
+    else:
+        return b
 
 
 
-
-
-
-
-
-
-
-
-"""
-    if content_name == '별점 확인&주기' or "학식(중식)" or "학식(석식)" or "기숙사(조식)" or "기숙사(중식)" or "기숙사(석식)" or "처음으로":
-        input_text = button.star_check(content_name)
-
-        if input_text == "별점":
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '평가할 식단을 선택하세요' #+ str(content_name_jeon)
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["학식(중식)", "학식(석식)", "기숙사(조식)", "기숙사(중식)", "기숙사(석식)", "처음으로"]
-                    }
-                }
-            )
-        elif input_text == "채움이룸_중식":
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '별점을 매겨주세요'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
-                    }
-                }
-            )
-        elif input_text == "채움이룸_석식":
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '별점을 매겨주세요'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
-                    }
-                }
-            )
-        elif input_text == "기숙사_조식":
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '별점을 매겨주세요'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
-                    }
-                }
-            )
-        elif input_text == "기숙사_중식":
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '별점을 매겨주세요'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
-                    }
-                }
-            )
-        elif input_text == "기숙사_석식":
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '별점을 매겨주세요'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★", "처음으로"]
-                    }
-                }
-            )
-        else:
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': input_text
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["채움관", "이룸관", "기숙사", "별점 확인&주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
-                    }
-                }
-            )
-    elif content_name == '채움관' :
-        input_text = button.button_check(content_name)
-
-        if input_text == '내일':
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': '식당을 선택'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["채움관(내일)", "이룸관(내일)", "기숙사(내일)", "처음으로"]
-                    }
-                }
-            )
-        else:
-            return JsonResponse(
-                {
-                    'message': {
-                        'text': input_text
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["채움관", "이룸관", "기숙사", "별점 확인&주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
-                    }
-                }
-            )
-
-
-
-"""
+    """
