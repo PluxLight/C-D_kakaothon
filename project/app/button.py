23ca22d46998d28669c24fe6a27b3e81a254d8c3
PluxLight <aeoragy@gmail.com>
2018-11-06 00:42:16 +0900

    문구 양식 통일 /  별점 등록 조건 추가  (식사기간에만 가능)

--- project/app/button.py
+++ project/app/button.py
@@ -2,6 +2,7 @@ from app import crawcafeteria
 from app import weather
 from app import db_control
 from django.http import JsonResponse
+import datetime
 
 
 class message_make:
@@ -42,7 +43,7 @@ class message_make:
             message_val = JsonResponse(
                 {
                     'message': {
-                        'text': '식당을 선택'
+                        'text': '식당을 선택하세요'
                     },
                     "keyboard": {
                         "type": "buttons",
@@ -54,7 +55,7 @@ class message_make:
             message_val = JsonResponse(
                 {
                     'message': {
-                        'text': '식당을 선택'
+                        'text': '별점은 중복 투표가 불가능하며\n식사시간 중에만 투표가 가능합니다\n식당을 선택하세요'
                     },
                     "keyboard": {
                         "type": "buttons",
@@ -66,11 +67,11 @@ class message_make:
             message_val = JsonResponse(
                 {
                     'message': {
-                        'text': '식단을 선택'
+                        'text': '식단을 선택하세요'
                     },
                     "keyboard": {
                         "type": "buttons",
-                        "buttons": ["조식", "중식", "석식", "처음으로"]
+                        "buttons": ["중식", "석식", "처음으로"]
                     }
                 }
             )
@@ -78,7 +79,7 @@ class message_make:
             message_val = JsonResponse(
                 {
                     'message': {
-                        'text': '식단을 선택'
+                        'text': '식단을 선택하세요'
                     },
                     "keyboard": {
                         "type": "buttons",
@@ -87,17 +88,167 @@ class message_make:
                 }
             )
         elif self.cur_text == '조식' or self.cur_text == '중식' or self.cur_text == '석식':
-            message_val = JsonResponse(
-                {
-                    'message': {
-                        'text': '별점을 선택'
-                    },
-                    "keyboard": {
-                        "type": "buttons",
-                        "buttons": ["★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆", "처음으로"]
+
+            now_time = datetime.datetime.now()
+
+            chaeum_lunch_start = now_time.replace(hour=11, minute=50, second=0, microsecond=0)
+            chaeum_lunch_end = now_time.replace(hour=13, minute=30, second=0, microsecond=0)
+
+            chaeum_dinner_start = now_time.replace(hour=16, minute=50, second=0, microsecond=0)
+            chaeum_dinner_end = now_time.replace(hour=18, minute=30, second=0, microsecond=0)
+
+            domitory_morning_start = now_time.replace(hour=7, minute=30, second=0, microsecond=0)
+            domitory_morning_end = now_time.replace(hour=9, minute=0, second=0, microsecond=0)
+
+            domitory_lunch_start = now_time.replace(hour=12, minute=0, second=0, microsecond=0)
+            domitory_lunch_end = now_time.replace(hour=13, minute=30, second=0, microsecond=0)
+
+            domitory_dinner_start = now_time.replace(hour=18, minute=0, second=0, microsecond=0)
+            domitory_dinner_end = now_time.replace(hour=19, minute=30, second=0, microsecond=0)
+
+            #채움관 중식 별점 등록 가능여부 확인
+            if self.cur_text == '중식' and self.pre_text == '채움관&이룸관' and (now_time >=chaeum_lunch_start and now_time <= chaeum_lunch_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 선택하세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆", "처음으로"]
+                        }
                     }
-                }
-            )
+                )
+            elif self.cur_text == '중식' and self.pre_text == '채움관&이룸관' and (now_time < chaeum_lunch_start or now_time > chaeum_lunch_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '현재는 별점 등록기간이 아닙니다\n처음으로 돌아갑니다'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+
+            # 채움관 석식 별점 등록 가능여부 확인
+            elif self.cur_text == '석식' and self.pre_text == '채움관&이룸관' and (now_time >=chaeum_dinner_start and now_time <= chaeum_dinner_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 선택하세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆", "처음으로"]
+                        }
+                    }
+                )
+            elif self.cur_text == '석식' and self.pre_text == '채움관&이룸관' and (now_time < chaeum_dinner_start or now_time > chaeum_dinner_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '현재는 별점 등록기간이 아닙니다\n처음으로 돌아갑니다'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+
+            # 기숙사 조식 별점 등록 가능여부 확인
+            elif self.cur_text == '조식' and self.pre_text == '기숙사' and (now_time >=domitory_morning_start and now_time <= domitory_morning_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 선택하세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆", "처음으로"]
+                        }
+                    }
+                )
+            elif self.cur_text == '조식' and self.pre_text == '기숙사' and (now_time < domitory_morning_start or now_time > domitory_morning_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '현재는 별점 등록기간이 아닙니다\n처음으로 돌아갑니다'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+
+            # 기숙사 중식 별점 등록 가능여부 확인
+            elif self.cur_text == '중식' and self.pre_text == '기숙사' and (now_time >=domitory_lunch_start and now_time <= domitory_lunch_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 선택하세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆", "처음으로"]
+                        }
+                    }
+                )
+            elif self.cur_text == '중식' and self.pre_text == '기숙사' and (now_time < domitory_lunch_start or now_time > domitory_lunch_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '현재는 별점 등록기간이 아닙니다\n처음으로 돌아갑니다'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+
+            #기숙사 석식 별점 등록 가능여부 확인
+            elif self.cur_text == '석식' and self.pre_text == '기숙사' and (now_time >=domitory_dinner_start and now_time <= domitory_dinner_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '별점을 선택하세요'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆", "처음으로"]
+                        }
+                    }
+                )
+            elif self.cur_text == '석식' and self.pre_text == '기숙사' and (now_time < domitory_dinner_start or now_time > domitory_dinner_end):
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '현재는 별점 등록기간이 아닙니다\n처음으로 돌아갑니다'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+
+            else:
+                message_val = JsonResponse(
+                    {
+                        'message': {
+                            'text': '오류가 발생했습니다\n처음으로 돌아갑니다'
+                        },
+                        "keyboard": {
+                            "type": "buttons",
+                            "buttons": ["채움관", "이룸관", "기숙사", "별점 주기", "양식당", "오늘의 날씨", "내일의 메뉴 확인"]
+                        }
+                    }
+                )
+
         elif self.cur_text == '★★★★★' or self.cur_text == "★★★★☆" or self.cur_text == '★★★☆☆' or \
                 self.cur_text == '★★☆☆☆' or self.cur_text == '★☆☆☆☆':
             if db_control.overlap_check(self.user_key, self.pre_text, self.pre_pre_text) == 0: #해당 항목에 투표를 한번도 안했을경우
@@ -127,7 +278,7 @@ class message_make:
                 message_val = JsonResponse(
                     {
                         'message': {
-                            'text': '별점의 중복 투표는 제한되어 있습니다.'
+                            'text': '별점의 중복 투표는 제한되어 있습니다'
                         },
                         "keyboard": {
                             "type": "buttons",
