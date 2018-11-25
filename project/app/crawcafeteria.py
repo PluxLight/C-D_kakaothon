486a0a09de91eef2a14bdd2d4363057b9eb6ced0
PluxLight <aeoragy@gmail.com>
2018-11-25 23:08:47 +0900

    맘스터치 정보 전달하는 기능 추가

--- project/app/crawcafeteria.py
+++ project/app/crawcafeteria.py
@@ -119,11 +119,16 @@ def erum_tomorrow(): #이움관 익일 정보
         return data
 
 
-def restaurant():
+def restaurant(): #양식당 정보
     rest_data = "운영시간 10:00 ~ 19:00\n(주말, 공휴일 제외)\n\n김밥: 1500원\n참치김밥: 1500원\n우동: 2500원\n참치마요: 3500원\n등심돈가스: 3800원\n치킨까스: 3800원\n치즈돈가스: 4000원\n불닭덮밥: 3800원\n\
 스팸덮밥: 3800원\n샐러드파스타: 3800원\n돼지불고기: 4000원\n소고기불고기: 4500원\n오리불고기: 5000원\n\n"
 
     return rest_data
+
+def moms(moms_type): #맘스터치 정보
+    moms_data = db_control.moms_db(moms_type)
+
+    return moms_data
 
 #이룸관 구버전
 """
